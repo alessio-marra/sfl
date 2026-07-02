b6a20eb1a23ece5102ed1c332c10213f2a797f6b
alessio-marra <alessio.marra@sfl.ch>
2026-07-02 09:26:45 +0200

    Update check_scoreplay.py

--- check_scoreplay.py
+++ check_scoreplay.py
@@ -204,7 +204,7 @@ def send_email(new_items: list[dict]):
 
     recipients = [e.strip() for e in EMAIL_TO.split(",")]
     
-   with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
         smtp.login(EMAIL_FROM, EMAIL_PASSWORD)
         smtp.sendmail(EMAIL_FROM, recipients, msg.as_string())
 
